d07a76b2ebb2f6b3b34ca97fd40a5cc1d90bb8ad
Isaias Mendes <isaiaszmendes@gmail.com>
2023-04-10 17:55:32 -0300

    mock aula

--- server.js
+++ server.js
@@ -6,8 +6,25 @@ const app = express();
 app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
-const proprietarios = [];
-const pets = [];
+const proprietarios = [
+  {
+		"id": "cf5af97e-24f5-499b-940e-29a792acba79",
+		"nome": "biel",
+		"cpf": "14340478509",
+		"telefone": "11940800022"
+	}
+];
+
+const pets = [
+  {
+		"id": "2a1fb52b-7b83-45a9-87bf-76a7ba005136",
+		"nome": "spike",
+		"proprietarioId": "cf5af97e-24f5-499b-940e-29a792acba79",
+		"idade": 6,
+		"peso": 20,
+		"raca": "border collie"
+	}
+];
 
 // cadastrar proprietario
 app.post('/proprietarios', (request, response) => {
